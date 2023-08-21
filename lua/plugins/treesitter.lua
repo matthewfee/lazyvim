bff8e12d3b014e44151c8dd3a8390f7289e74074
Matthew Fee <matthewefee@gmail.com>
2023-08-21 20:18:35 +0200

    --del: unneeded overrides

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,6 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
-        "vue",
-      })
-
-      opts.autotag = {
-        enabled = true,
-      }
-    end,
-  },
-  {
     "windwp/nvim-ts-autotag",
+    opts = {},
   },
 }
